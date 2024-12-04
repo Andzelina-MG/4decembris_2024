e756ede055c571408c4acf9c9f1b38b3730ea339
Andzelina-MG <andzelina.mogolona-gutieresa@grobinasskola.lv>
2024-12-04 14:29:27 +0200

    Ierakstu komentārus 1. uzd.

--- 01/main.py
+++ 01/main.py
@@ -1,11 +1,11 @@
-""" Insert Name """
-name = input("Whats your name: ")
+""" Insert Name """ # Ievada vārdu
+name = input("Whats your name: ") # Jautā lietotāja vārdu
 
-""" Insert age. Also convert age into an Integer value """
-age = int(input("How old are you: "))
+""" Insert age. Also convert age into an Integer value """ # Ievada vecumu
+age = int(input("How old are you: ")) # Jautā lietotāja vecumu - cik ir gadi
 
-""" Do the math """
-year = str((2018 - age)+100)
+""" Do the math """ # Izrēķina rezultātu
+year = str((2018 - age)+100) # Aprēķina kurā gadā lietotājam būs 100 gadi
 
-""" Print the result """
-print(name + " will be 100 years old in the year " + year)
+""" Print the result """ # Parāda rezultātu
+print(name + " will be 100 years old in the year " + year) # Rezultāts parādās terminālī
