37c3e0c5989ea06033a3ba765bf064134defdcd5
Andzelina-MG <andzelina.mogolona-gutieresa@grobinasskola.lv>
2024-12-04 14:35:52 +0200

    Ierakstu komentārus 2. un 3. uzd.

--- 02/main.py
+++ 02/main.py
@@ -1,24 +1,26 @@
-""" Method 1 """
+""" Method 1 """ # 1. metode
 
-num = int(input("Give me a number to check: "))
-check = int(input("Give me a number to divide by: "))
+num = int(input("Give me a number to check: ")) # Jautā lietotājam skaitli, ko pārbaudīt
+check = int(input("Give me a number to divide by: ")) # Jautā lietotājam skaitli ar ko dalīt
 
-if num % 4 == 0:
+if num % 4 == 0: # Ja skaitļa dalīšanas atlikums ar 2 ir 0
     print(num, "is a multiple of 4")
-elif num % 2 == 0:
-    print(num, "is an even number")
+elif num % 2 == 0: # Ja skaitļa dalīšanas atlikums ar 2 ir 0
+    print(num, "is an even number") # Izvada terminālī, ka ir pāra skaitlis
 else:
-    print(num, "is an odd number")
+    print(num, "is an odd number") # Izvada terminālī, ka ir nepāra skaitlis
 
 if num % check == 0:
-    print(num, "divides evenly by", check)
+    print(num, "divides evenly by", check) # Izvada terminālī, ka skaitlis dalās
 else:
-    print(num, "doesn't divide evenly by", check)
+    print(num, "doesn't divide evenly by", check) # Izvada terminālī, ka skaitlis nedalās
 
-""" Method 2 """
-num = int(input("Enter a number: "))
+
+""" Method 2 """ # 2. metode
+
+num = int(input("Enter a number: ")) # Jautā lietotājam ievadīt skaitli
 mod = num % 2
 if mod > 0:
-    print("You picked an odd number.")
+    print("You picked an odd number.") # Izvada terminālī, ka lietotājs izvēlējās nepāra
 else:
-    print("You picked an even number.")
+    print("You picked an even number.") # Izvada terminālī, ka lietotājs izvēlējās pāra skaitli
--- 03/main.py
+++ 03/main.py
@@ -1,11 +1,11 @@
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89] # Izveido sarakstu
 
-num = int(input("Choose a number: "))
+num = int(input("Choose a number: ")) # Jautā lietotājam izvēlēties ciparu
 
-new_list = []
+new_list = [] # Izveido jaunu sarakstu
 
 for i in a:
-    if i < num:
-        new_list.append(i)
+    if i < num: # Pārbauda vai i ir mazāks par skaitli
+        new_list.append(i) # Pievieno jaunu sarakstu
 
-print(new_list)
+print(new_list) # Jaunais saraksts tiek parādīts terminālī
